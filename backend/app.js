77ca7d488b139fe92b5f9232cc6cde7a89a0549f
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-04-17 11:58:34 +0300

    creating sign in and sign up APIs

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import jobRoutes from './routes/jobRoutes.js';
+import authRoutes from "./routes/authRoutes.js"
 
 const app= express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/jobs', jobRoutes);
 app.use('/api/getJobs', jobRoutes);
+app.use('/api/auth', authRoutes);
 
 
 export default app;
---
+++ backend/controllers/authController.js
@@ -0,0 +1,44 @@
+import User from '../models/user.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const signupUser=async (req, res)=>{
+    const{name, phoneNumber, email, password}=req.body;
+    try {
+console.log('User:',req.body);
+
+        const existingUser= await User.findOne({email});
+        if (existingUser) {
+            return res.status(201).json({'error': 'Email is already exist.'});
+        }
+        const hashedPassword= await bcrypt.hash(password,10);
+        const user=await User.create({name, phoneNumber, email, password:hashedPassword});
+        res.status(200).json({message:"User crated successfully!"});
+    } catch (error) {
+        res.status(500).json({error:error.toString()});
+    }
+};
+
+export const loginUser=async (req,res)=>{
+    const {email,password}=req.body;
+    console.log('user:',req.body);
+
+    try {
+        const user=await User.findOne({email});
+        if (!user) {
+            console.log('User not found', user);
+
+            return res.status(201).json({error:'User not found.'});
+        }
+        const loginMatch=await bcrypt.compare(password, user.password);
+        if (!loginMatch) {
+            return res.status(201).json({
+                error:"Invalid credentials."
+            });
+        }
+        const token= await jwt.sign({id:user._id, },process.env.JWT_SECRET,{ expiresIn: '1d'});
+        res.status(200).json({message:"User login successfully!"});
+    } catch (error) {
+res.status(201).json({error:error.toString()});
+    }
+}
