11d2b27f8ef4294fd4004c4a1ff4a381211caf12
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-04-30 11:52:30 +0300

    working job tab section

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,7 +1,17 @@
-const Jobs=()=>(
-    <div className="text-2xl font-semibold text grey-800">
-        Browse Available Jobs
+import React from 'react'
+import HeroSection from '../components/HeroSection';
+import FilterSidebar from '../components/FilterSidebar';
+import JobList from '../components/JobList';
+
+const Jobs=()=> {
+   return     (
+    <div>
+        <HeroSection />
+    <div className="container mx-auto px-4 py-8 flex space-x-6">
+<FilterSidebar />
+<JobList />
+    </div>
 
     </div>
-);
+);}
 export default Jobs;
