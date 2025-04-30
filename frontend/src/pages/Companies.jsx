dc1b6f66a63825561667377ba4b36e405bac940c
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-04-30 11:21:01 +0300

    Adding tab components

---
+++ frontend/src/pages/Companies.jsx
@@ -0,0 +1,7 @@
+const Companies=()=>(
+    <div className="text-2xl fnt-semibold text-gray-800">
+Explorer Companies
+    </div>
+);
+
+export default Companies;
---
+++ frontend/src/pages/Jobs.jsx
@@ -0,0 +1,7 @@
+const Jobs=()=>(
+    <div className="text-2xl font-semibold text grey-800">
+        Browse Available Jobs
+
+    </div>
+);
+export default Jobs;
